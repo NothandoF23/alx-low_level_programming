bdd3e6f846f1ee390e3cf4ded0d7dac2bb2b48e0
Nothando Duma <fai231kalwandle@gmail.com>
2023-06-15 13:38:51 -0700

    print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *Main function that returns a value of 0 : success
- *And prints out text using the put function
+ * main - prints out text using the put function
+ * Return: 0 (on success)
  */
 
 int main(void)
 	{
-
 	puts(""Programming is like building a multilingual puzzle"");
-
 	return (0);
 }
