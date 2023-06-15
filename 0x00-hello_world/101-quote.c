99831697fcee0242b54838737d334528420eb029
Nothando Duma <fai231kalwandle@gmail.com>
2023-06-15 15:03:35 -0700

    print text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints text to stderr using the fput function
+ * Return: 1 (on success)
+ */
+
+int main(void)
+	{
+	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
+
