cd2205ab3517dc9efc84fef3796be21e19ef8f78
Nothando Duma <fai231kalwandle@gmail.com>
2023-06-15 13:52:31 -0700

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints out text using the printf function
+ * Return: 0 (on success)
+ */
+
+int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
