60c06504da0f78a975970b6949ad3c3b68c35e99
Nothando Duma <fai231kalwandle@gmail.com>
2023-06-15 16:13:30 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,8 @@ int main(void)
 	printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %1u byte(s)\n", (unsigned long) sizeof(long int));
-	printf("Size of a long long int: %1u byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(float));
 
 	return (0);
 }
-
-
