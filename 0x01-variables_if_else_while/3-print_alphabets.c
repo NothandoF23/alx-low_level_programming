d02420503fdf6c974ee0c46a9f46f4061c0001b7
Nothando Duma <fai231kalwandle@gmail.com>
2023-06-18 10:09:55 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - contains a program that prints the alphabet letters
+ * in lowercases followed by uppercases
+ * Return: 0 (after execution)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar (letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar (letter);
+	}
+	putchar ('\n');
+	return (0);
