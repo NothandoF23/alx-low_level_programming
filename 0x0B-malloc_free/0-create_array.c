4f84837fd7cb796af6915ca3e968763919f50170
Nothando Duma <fai231kalwandle@gmail.com>
2023-07-11 11:52:06 -0700

    added loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,19 +15,18 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 
-	if (size == 0)
+	if (size != 0)
 	{
-		return (NULL);
+		a = malloc(size * sizeof(char));
+		if (a == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < size; i++)
+		{
+			a[i] = c;
+			return (a);
+		}
 	}
-
-	a = malloc(size * sizeof(char));
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		a[i] = c;
-		return (a);
-	}
+	return (NULL);
 }
