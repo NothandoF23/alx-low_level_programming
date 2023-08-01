a76b9aa4850972153f0233ba08ecfb17385723fc
Nothando Duma <fai231kalwandle@gmail.com>
2023-08-01 14:07:18 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint_t *tmp = NULL;
+	listint_t *node;
 	unsigned int i = 0;
 
 	if (*head == NULL)
@@ -26,16 +26,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if (!temp || !(temp->next))
-			return (-1);
 		temp = temp->next;
 		i++;
 	}
-
-
-	tmp = temp->next;
-	temp->next = tmp->next;
-	free(tmp);
+	node = temp->next;
+	temp->next = node->next;
+	free(node);
 
 	return (1);
 }
