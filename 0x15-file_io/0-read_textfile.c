8820f44f7a9f70cc4c125458e712b8a77ac3b033
Nothando Duma <fai231kalwandle@gmail.com>
2023-08-07 13:07:09 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -29,17 +29,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	i = open(filename, O_RDONLY);
 	if (i == -1)
+	{
 		return (0);
-
+	}
 	j = read(i, fl, letters);
-	if (j == -1)
-		return (0);
-
-
 	len = write(STDOUT_FILENO, fl, j);
-	if (len == -1)
-		return (0);
-
 	free(fl);
 	close(i);
 	return (len);
